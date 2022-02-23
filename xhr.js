66520de53cb842c910fb7e644fb9797931995b6f
Gulbara Satybekova <gulbara.satybekova.on@gmail.com>
2022-02-23 22:30:58 +0600

    modified md file

--- xhr.js
+++ xhr.js
@@ -22,3 +22,35 @@ function loadData() {
 
   xhr.send();
 }
+
+// document.getElementById("get-news").addEventListener("click", loadData);
+
+// function loadData() {
+//   const xhr = new XMLHttpRequest();
+
+//   xhr.open("GET", "news.txt", true);
+
+//   xhr.onload = function () {
+//     if (this.status === 200) {
+
+//       const news = JSON.parse(this.response);
+
+//       let output = document.createElement(ul);
+
+//       news.forEach(new => {
+//         output += `
+//            <li>
+//             ${this.new[]}
+//             </li>
+//             `;
+// });
+    
+//       document.querySelector(".output").innerHTML = output;
+// }
+//   };
+
+//   xhr.send();
+// };
+
+
+
